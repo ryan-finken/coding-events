cf4fa10a11528f2a62325fd9700ecd2747da49c4
Ryan Finken <ryanfinken338@gmail.com>
2021-06-24 14:30:15 -0500

    will finish this later

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,38 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class EventCategory {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank(message = "Name is required.")
+    private String name;
+
+    public EventCategory(String name) {
+        this.name = name;
+    }
+
+    public EventCategory() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
